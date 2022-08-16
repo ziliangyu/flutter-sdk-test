4d3e37d53e086b77519677b8057f822a311aa10b
yuziliang <yuziliangvictory@gmail.com>
2022-08-16 12:00:18 +0800

    feat: ci

--- test/flutter_sdk_test_test.dart
+++ test/flutter_sdk_test_test.dart
@@ -4,16 +4,16 @@ import 'package:flutter_sdk_test/flutter_sdk_test_platform_interface.dart';
 import 'package:flutter_sdk_test/flutter_sdk_test_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockFlutterSdkTestPlatform 
+class MockFlutterSdkTestPlatform
     with MockPlatformInterfaceMixin
     implements FlutterSdkTestPlatform {
-
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
 }
 
 void main() {
-  final FlutterSdkTestPlatform initialPlatform = FlutterSdkTestPlatform.instance;
+  final FlutterSdkTestPlatform initialPlatform =
+      FlutterSdkTestPlatform.instance;
 
   test('$MethodChannelFlutterSdkTest is the default instance', () {
     expect(initialPlatform, isInstanceOf<MethodChannelFlutterSdkTest>());
@@ -23,7 +23,7 @@ void main() {
     FlutterSdkTest flutterSdkTestPlugin = FlutterSdkTest();
     MockFlutterSdkTestPlatform fakePlatform = MockFlutterSdkTestPlatform();
     FlutterSdkTestPlatform.instance = fakePlatform;
-  
+
     expect(await flutterSdkTestPlugin.getPlatformVersion(), '42');
   });
 }
